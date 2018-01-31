cb9c918dbe2399fbe855df9c46da3166dda6679a
Marko Djurdjev <marko.djurdjev93@gmail.com>
2018-01-31 19:34:39 +0100

    dodao dialog

--- app/src/main/java/com/example/code/dialogexamples/MainActivity.java
+++ app/src/main/java/com/example/code/dialogexamples/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.code.dialogexamples;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showSimpleDialogOne(View view){
+        SimpleDialogOne simpleDialogOne = new SimpleDialogOne();
+        simpleDialogOne.show(getFragmentManager(), null);
     }
 }
